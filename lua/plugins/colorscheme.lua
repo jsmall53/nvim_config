49b02bbc8ecfba0985ee0ddbabb32efdf3f647f6
jsmall53 <jsmall53@ymail.com>
2025-04-21 14:52:02 -0700

    switching to gruvbox theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,36 @@
+-- return {
+--     --'folke/tokyonight.nvim',
+--     --priority = 1000, -- Make sure to load this before all the other start plugins.
+--     --config = function()
+--        --require('tokyonight').setup {
+--         --styles = {
+--           --comments = { italic = false }, -- Disable italics in comments
+--         --},
+--        --}
+--
+--       --vim.cmd.colorscheme 'tokyonight-night'
+--     --end,
+--     "rose-pine/neovim",
+--     name = "rose-pine",
+--    	config = function()
+--         require("rose-pine").setup({
+--             variant = "main",
+--             dark_variant = "main",
+--             styles = {
+--                 italic = false
+--             }
+--         })
+--         vim.cmd("colorscheme rose-pine")
+-- 	end
+-- }
 return {
-    --'folke/tokyonight.nvim',
-    --priority = 1000, -- Make sure to load this before all the other start plugins.
-    --config = function()
-       --require('tokyonight').setup {
-        --styles = {
-          --comments = { italic = false }, -- Disable italics in comments
-        --},
-       --}
-      
-      --vim.cmd.colorscheme 'tokyonight-night'
-    --end,
-    "rose-pine/neovim",
-    name = "rose-pine",
-   	config = function()
-        require("rose-pine").setup({
-            variant = "main",
-            dark_variant = "main",
-            styles = {
-                italic = false
-            }
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    priority = 1000,
+    config = function()
+        require("gruvbox").setup({
         })
-        vim.cmd("colorscheme rose-pine")
-	end
---    "vague2k/vague.nvim",
---    config = function()
---        require("vague").setup({
---        })
---    end
+        vim.cmd("colorscheme gruvbox")
+    end
+
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,8 @@ return {
 	'nvim-treesitter/nvim-treesitter',
 	build = ':TSUpdate',
 	config = function()
-		require("nvim-treesitter.install").compilers = { "clang", "zig" } -- USE THIS ONE FOR WINDOWS
-		--require("nvim-treesitter.install").compilers = { "gcc", "zig", "clang" } -- USE THIS ONE FOR LINUX
+		--require("nvim-treesitter.install").compilers = { "clang", "zig" } -- USE THIS ONE FOR WINDOWS
+		require("nvim-treesitter.install").compilers = { "gcc", "zig", "clang" } -- USE THIS ONE FOR LINUX
 		require("nvim-treesitter.configs").setup({
 			ignore_install = { "lua" },
 			ensure_installed = { "c", "rust", "vim", "vimdoc", "markdown", "javascript", "typescript", "python" },
