7b80e5cd94d5c7995890ac9f7eda3851cbca71c9
jsmall53 <jsmall53@ymail.com>
2025-05-02 16:47:43 -0700

    indent-lines

--- init.lua
+++ init.lua
@@ -2,3 +2,5 @@ require("config.keymap")
 require("config.options")
 require("config.lazy")
 
+-- For disabling inline diagnostic information
+-- vim.diagnostic.config({ virtual_text = false })
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,6 +1,9 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = { 
+        theme = 'gruvbox',
+    },
     config = function()
         require("lualine").setup({})
     end
